cdb9c88f622f44cf2b929e7f9e4d18f280d01df9
MarieVanGreves <keiiskyx@gmail.com>
2023-05-11 16:39:36 +0200

    Adding the NavBar

--- Blog-de-Frictine/src/App.jsx
+++ Blog-de-Frictine/src/App.jsx
@@ -1,5 +1,13 @@
-import "./App.css";
+import React from 'react';
+import Home from "../src/pages/Accueil"
+import styles from "../src/App.module.css"
 
-function App() {}
+function App() {
+  return (
+    <div>
+        <Home />
+    </div>
+  );
+}
 
 export default App;
---
+++ Blog-de-Frictine/src/pages/Accueil.jsx
@@ -0,0 +1,14 @@
+import NavBar from "./../components/NavBar/NavBar";
+
+
+
+function Home() {
+    return (
+        <div>
+        <NavBar />
+        </div>
+    )
+}
+
+
+export default Home;
